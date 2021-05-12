9f47273400c84247239a0fca3de9b0461ef27d5f
Peter <peter.pilestedt@gmail.com>
2021-05-12 14:43:04 +0200

    adds MyMathCalc

---
+++ src/main/java/com/example/springdocker/MyMathCalc.java
@@ -0,0 +1,14 @@
+package com.example.springdocker;
+
+public class MyMathCalc {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int sub(int a, int b){
+        return a - b;
+    }
+    public double div(double a, double b){
+        return a/b;
+    }
+}
