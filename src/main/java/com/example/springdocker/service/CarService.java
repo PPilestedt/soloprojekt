90e9d5f695407b8b7440b85243f1e748f7430ea4
Peter <peter.pilestedt@gmail.com>
2021-05-14 14:56:16 +0200

    Refactor method name

--- src/main/java/com/example/springdocker/service/CarService.java
+++ src/main/java/com/example/springdocker/service/CarService.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class CarService {
     private final CarRepository repository;
 
-    public List<Car> getFoods() {
+    public List<Car> getCars() {
         return repository.findAll();
     }
 
